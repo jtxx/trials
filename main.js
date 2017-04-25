7e128271f286933b4c52a86b954cd679ca8e1118
jtxx <joelt.tirado@gmail.com>
2017-04-25 16:34:38 -0400

    initializing tutorial repository

---
+++ main.js
@@ -0,0 +1,18 @@
+var dataset = [];
+
+for (var i = 0; i < 25; i++) {
+	var newNumber = Math.round(Math.random() * 30);
+	dataset.push(newNumber);
+}
+		
+d3.select("body").selectAll("div")
+	.data(dataset)
+	.enter()
+	.append("div")
+	.attr("class", "bar")
+	.style("height", function(d) {
+		var barHeight = d * 5; 
+		return barHeight + "px";
+});
+
+console.log(dataset);
